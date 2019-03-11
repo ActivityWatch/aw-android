09c15886ed7b5909644fa8042b94440ab41879c5
Erik Bjäreholt <erik@bjareho.lt>
2019-03-11 12:43:49 +0100

    started using bundle for getting fastlane

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ set -e
 mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk aw-android.apk
 jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks aw-android.apk activitywatch
 zipalign -v 4 aw-android.apk aw-android-release.apk
-fastlane supply run -b aw-android-release.apk
+bundle exec fastlane supply run -b aw-android-release.apk
