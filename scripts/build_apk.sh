6a312be9cf2c083e49a212c9e0d23dbc1f288ee7
Erik Bjäreholt <erik@bjareho.lt>
2019-03-15 18:57:05 +0100

    fixed deployment (now also through GitHub releases)

---
+++ scripts/build_apk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+OUTPUT_NAME="aw-android.apk"
+
+if [ -z $storepass ]; then
+    echo "\$storepass and \$keypass need to be set"
+    exit 1
+fi
+
+./gradlew assembleRelease
+mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk $OUTPUT_NAME
+jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks $OUTPUT_NAME activitywatch
+jarsigner -verify $OUTPUT_NAME
+${ANDROID_HOME}/build-tools/*/zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
+mv $OUTPUT_NAME.new $OUTPUT_NAME
