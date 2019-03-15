e75ba3b7103174a74dc60dbdb4d9efa94a3b4beb
Erik Bjäreholt <erik@bjareho.lt>
2019-03-15 19:44:07 +0100

    giving this one more go

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 OUTPUT_NAME="aw-android.apk"
 
@@ -13,5 +13,5 @@ fi
 mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk $OUTPUT_NAME
 jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks $OUTPUT_NAME activitywatch
 jarsigner -verify $OUTPUT_NAME
-${ANDROID_HOME}/build-tools/*/zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
+$ANDROID_HOME/build-tools/28.*/zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
 mv $OUTPUT_NAME.new $OUTPUT_NAME
