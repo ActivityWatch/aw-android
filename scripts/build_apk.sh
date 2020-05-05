81e838b5d056e816c34e874ec4fcad39ee3d3fcf
Erik Bjäreholt <erik@bjareho.lt>
2020-05-05 17:01:30 +0200

    ci: fixed looking up location of zipalign

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -4,6 +4,11 @@ set -xe
 
 OUTPUT_NAME="aw-android.apk"
 
+if [ -z $ANDROID_HOME ]; then
+    echo "\$ANDROID_HOME needs to be set"
+    exit 1
+fi
+
 if [ -z $storepass ]; then
     echo "\$storepass and \$keypass need to be set"
     exit 1
@@ -13,5 +18,10 @@ fi
 mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk $OUTPUT_NAME
 jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks $OUTPUT_NAME activitywatch
 jarsigner -verify $OUTPUT_NAME
-$ANDROID_HOME/build-tools/28.*/zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
+
+pattern="$ANDROID_HOME/build-tools/*/zipalign"
+zipalign_locs=( $pattern )
+zipalign="${zipalign_locs[0]}"
+echo $zipalign_locs
+$zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
 mv $OUTPUT_NAME.new $OUTPUT_NAME
