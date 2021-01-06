54dccb917be882cad62f1da8bb17d0064aeabc11
Erik Bjäreholt <erik@bjareho.lt>
2021-01-06 16:06:55 +0100

    ci: simplified build_apk.sh script

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xe
+set -e
 
 OUTPUT_NAME="aw-android.apk"
 
@@ -19,9 +19,6 @@ mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk $OUTPUT_NAME
 jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks $OUTPUT_NAME activitywatch
 jarsigner -verify $OUTPUT_NAME
 
-pattern="$ANDROID_HOME/build-tools/*/zipalign"
-zipalign_locs=( $pattern )
-zipalign="${zipalign_locs[0]}"
-echo $zipalign_locs
+zipalign=$(find $ANDROID_HOME/build-tools -name "zipalign" -print | head -n 1)
 $zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
 mv $OUTPUT_NAME.new $OUTPUT_NAME
