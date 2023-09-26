5ebd7c213e0739943aca5d31035aba184519dd38
Erik Bjäreholt <erik@bjareho.lt>
2023-09-26 13:30:37 +0200

    build: switched from jarsigner to apksigner

--- scripts/sign_apk.sh
+++ scripts/sign_apk.sh
@@ -21,13 +21,20 @@ if [ -z $JKS_KEYPASS ]; then
     exit 1
 fi
 
-# Sign
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
-    -storepass $JKS_STOREPASS -keypass $JKS_KEYPASS \
-    -keystore android.jks $input activitywatch
-jarsigner -verify $input
-mv $input $output
-
+# Zipalign
 zipalign=$(find $ANDROID_HOME/build-tools -name "zipalign" -print | head -n 1)
-$zipalign -v 4 $output $output.new
-mv $output.new $output
+$zipalign -v 4 $input $input.new
+mv $input.new $input
+
+# Sign
+# Using apksigner instead of jarsigner since API 30+: https://stackoverflow.com/a/69473649
+apksigner=$(find $ANDROID_HOME/build-tools -name "apksigner" -print | head -n 1)
+$apksigner sign --ks android.jks --ks-key-alias activitywatch \
+    --ks-pass env:JKS_STOREPASS --key-pass env:JKS_KEYPASS \
+    $input
+
+# Verify
+$apksigner verify $input
+
+# Move to output destination
+mv $input $output
