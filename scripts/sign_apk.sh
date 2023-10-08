8c3c52496f9c80d129f9ebf346a65287c5752cee
Erik Bjäreholt <erik@bjareho.lt>
2023-10-08 15:54:28 +0200

    build: added bundle (.aab) building

--- scripts/sign_apk.sh
+++ scripts/sign_apk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Signs APKs or AABs using the android.jks keystore
+
 set -e
 
 input=$1
@@ -22,19 +24,31 @@ if [ -z $JKS_KEYPASS ]; then
 fi
 
 # Zipalign
-zipalign=$(find $ANDROID_HOME/build-tools -name "zipalign" -print | head -n 1)
-$zipalign -v -p 4 $input $input.new
-mv $input.new $input
+# Not needed for AABs
+if [[ $input == *.apk ]]; then
+    zipalign=$(find $ANDROID_HOME/build-tools -name "zipalign" -print | head -n 1)
+    $zipalign -v -p 4 $input $input.new
+    mv $input.new $input
+fi
 
 # Sign
-# Using apksigner instead of jarsigner since API 30+: https://stackoverflow.com/a/69473649
-apksigner=$(find $ANDROID_HOME/build-tools -name "apksigner" -print | head -n 1)
-$apksigner sign --ks android.jks --ks-key-alias activitywatch \
-    --ks-pass env:JKS_STOREPASS --key-pass env:JKS_KEYPASS \
-    $input
+# Using apksigner for APKs instead of jarsigner since API 30+: https://stackoverflow.com/a/69473649
+# Using jarsigner for AABs since apksigner doesn't support them
+if [[ $input == *.apk ]]; then
+    apksigner=$(find $ANDROID_HOME/build-tools -name "apksigner" -print | head -n 1)
+    $apksigner sign --ks android.jks --ks-key-alias activitywatch \
+        --ks-pass env:JKS_STOREPASS --key-pass env:JKS_KEYPASS \
+        $input
 
-# Verify
-$apksigner verify $input
+    # Verify
+    $apksigner verify $input
+fi
+if [[ $input == *.aab ]]; then
+    jarsigner -verbose \
+        -keystore android.jks activitywatch \
+        -storepass $JKS_STOREPASS -keypass $JKS_KEYPASS \
+        $input activitywatch
+fi
 
 # Move to output destination
 mv $input $output
